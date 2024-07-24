e85d79fd74de6dafa6c98bf981925671cdedf62f
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2024-07-24 05:55:39 -0700

    implemented some logic of authentication and service locator

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:spotify/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:spotify/presentation/intro/pages/get_started.dart';
 import 'package:spotify/presentation/splash/pages/splash.dart';
+import 'package:spotify/service_locator.dart';
 
 import 'core/configs/theme/app_theme.dart';
 import 'firebase_options.dart';
@@ -23,6 +24,7 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform
   );
 
+  await initilizeDependencies();
 
   runApp(const MyApp());
 }
